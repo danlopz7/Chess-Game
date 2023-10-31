347fed2ed118eced378b36cd7443147a355d7bb9
Daniel <daen.lopez14@gmail.com>
2023-10-30 19:18:32 -0600

    fixed revert commit

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -5,7 +5,7 @@ class Piece
   attr_accessor :available, :x, :y
   attr_reader :color
 
-  def initialize(_x, _y, _color)
+  def initialize(_available, _x, _y, _color)
     @available = true
     @x = _x
     @y = _y
@@ -13,7 +13,6 @@ class Piece
     #@board = _board
   end
 
-  # validStepApproach
   # Abstract method that should be implemented by subclasses
   def valid?(from_x, from_y, to_x, to_y)
     raise NotImplementedError, 'subclasses must implement this method'
